98f1dea2a8fcdf4bb88381945858b2bcb1bab5cf
Kanika <kanikarohilla06@gmail.com>
2026-06-08 11:42:26 +0530

    Day_8 assignment is uploaded

---
+++ Day_8/charactertri.java
@@ -0,0 +1,22 @@
+package Day_8;
+
+import java.util.Scanner;
+
+public class charactertri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows = ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) { // outer loop to iterate through each row
+            for (char ch = 'A'; ch < 'A' + i; ch++) { // inner loop to print the required number of characters in each row (ch < 'A' + i means that the number of characters increases with each row)
+                System.out.print(ch + " "); // print a character without moving to the next line
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    
+}
---
+++ Day_8/numbertri.java
@@ -0,0 +1,22 @@
+package Day_8;
+
+import java.util.Scanner;
+
+public class numbertri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows = ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) { // outer loop to iterate through each row
+            for (int j = 1; j <= i; j++) { // inner loop to print the required number of stars in each row (j <= i means that the number of stars increases with each row)
+                System.out.print(j + " "); // print a star without moving to the next line
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    
+}
